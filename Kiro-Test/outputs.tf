58899522a5945d0224b5176253e05d24855cdc90
sebas6596 <sebastian6596@hotmail.com>
2026-03-02 16:18:18 -0500

    chore: add Kiro-Test as a normal folder (remove submodule)

---
+++ Kiro-Test/outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.main.dns_name
+}
+
+output "alb_url" {
+  description = "URL of the Application Load Balancer"
+  value       = "http://${aws_lb.main.dns_name}"
+}
+
+output "web_server_ids" {
+  description = "Web server instance IDs"
+  value       = aws_instance.web[*].id
+}
